a673a19809df15a859e06b2431b8eefe33bc387c
RomanLeo2003 <112813754+RomanLeo2003@users.noreply.github.com>
2022-09-09 14:59:31 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,18 +1,15 @@
 import pickle
-import train
 import argparse
-parser = argparse.ArgumentParser(description='Train.py')
+from train import Model
+
+parser = argparse.ArgumentParser(description='Generate.py')
 parser.add_argument('--model', type=str, help='Dir for model pickle file')
 parser.add_argument('--prefix', type=str, help='Start-word')
-parser.add_argument('--length', type=int, help='Input dir for saving model')
-parser.add_argument()
+parser.add_argument('--length', type=int, help='Len of generate text')
 args = parser.parse_args()
 
-seq, char_to_idx, idx_to_char = train.vectorizer.text_to_seq()
-
-model_pickle = r"C:\Users\Роман\OneDrive\Рабочий стол\pickle_model.pkl"
 
 with open(args.model, 'rb') as file:
     model = pickle.load(file)
 
-print(model.generate(char_to_idx, idx_to_char, temp=0.3, prediction_len=args.length, start_text=args.prefix))
+print(model.generate(temp=0.3, prediction_len=args.length, start_text=args.prefix))
